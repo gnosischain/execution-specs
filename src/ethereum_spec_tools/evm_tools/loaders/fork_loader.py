c9ec963688677bb0a7e16489a78bcd6414d810f8
Chetna Mittal <chetna.mittal125@gmail.com>
2025-11-30 04:44:15 +0530

    feat: update t8n tool for gnosis (#6)

--- src/ethereum_spec_tools/evm_tools/loaders/fork_loader.py
+++ src/ethereum_spec_tools/evm_tools/loaders/fork_loader.py
@@ -80,6 +80,11 @@ class ForkLoad:
         return self._module("fork").process_unchecked_system_transaction
 
     @property
+    def process_block_rewards(self) -> Any:
+        """process_block_rewards function of the given fork."""
+        return self._module("fork").process_block_rewards
+
+    @property
     def process_withdrawals(self) -> Any:
         """process_withdrawals function of the given fork."""
         return self._module("fork").process_withdrawals
@@ -278,6 +283,11 @@ class ForkLoad:
     def trie_get(self) -> Any:
         """trie_get function of the fork."""
         return self._module("trie").trie_get
+
+    @property
+    def trie_set(self) -> Any:
+        """trie_set function of the fork."""
+        return self._module("trie").trie_set
 
     @property
     def hex_to_address(self) -> Any:
