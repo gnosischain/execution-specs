311f85483518483dbe179928858adb2f89ecf153
Chetna Miittal <chetna.mittal125@gmail.com>
2026-05-05 01:23:23 +0530

    chore: update block reward contract binary

--- packages/testing/src/execution_testing/forks/forks/src_contracts/block_reward_contract.sol
+++ packages/testing/src/execution_testing/forks/forks/src_contracts/block_reward_contract.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8.19;
 
 contract TestBlockReward {
-    function reward(address[] calldata, uint16[] calldata)
-        external pure returns(address[] memory, uint256[] memory)
-    {
+    /**
+     * Return ABI-encoded `(address[], uint256[])` where both arrays are empty:
+     * - offset to first array:  0x40
+     * - offset to second array: 0x60
+     * - first array length:     0
+     * - second array length:    0
+     */
+    fallback() external payable {
         assembly {
-            // Get free memory pointer
-            let ptr := mload(0x40)
-            
-            // Build ABI-encoded return data for two empty arrays
-            mstore(ptr, 0x40)                // Offset to first array (64 bytes)
-            mstore(add(ptr, 0x20), 0x60)     // Offset to second array (96 bytes)
-            mstore(add(ptr, 0x40), 0)        // First array length = 0
-            mstore(add(ptr, 0x60), 0)        // Second array length = 0
-            
-            // Return 128 bytes (4 words)
-            return(ptr, 0x80)
+            mstore(0x00, 0x40)
+            mstore(0x20, 0x60)
+            mstore(0x40, 0x00)
+            mstore(0x60, 0x00)
+            return(0x00, 0x80)
         }
     }
 }
