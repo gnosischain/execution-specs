c2a9d0e6e357d0505cf6b7b0fe654bfa73e63828
Chetna Miittal <chetna.mittal125@gmail.com>
2026-05-18 20:08:51 +0530

    Revert "Merge remote-tracking branch 'upstream/forks/amsterdam' into sync-upstream"
    This reverts commit 60d949ee3583cf73bf4f77b24184ff81a7647ff0, reversing
    changes made to cbdaee9cf1ad6d833173e1cf3d2c42c23f3345aa.

--- packages/testing/src/execution_testing/test_types/__init__.py
+++ packages/testing/src/execution_testing/test_types/__init__.py
@@ -3,7 +3,6 @@
 from .account_types import EOA, Alloc
 from .blob_types import Blob
 from .block_access_list import (
-    BalAccountAbsentValues,
     BalAccountChange,
     BalAccountExpectation,
     BalBalanceChange,
@@ -55,7 +54,6 @@ __all__ = (
     "DETERMINISTIC_FACTORY_ADDRESS",
     "Alloc",
     "AuthorizationTuple",
-    "BalAccountAbsentValues",
     "BalAccountChange",
     "BalAccountExpectation",
     "BalBalanceChange",
