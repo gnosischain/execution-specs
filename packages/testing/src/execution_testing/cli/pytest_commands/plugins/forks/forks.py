6dfb0888404c35768ed30860b050b2ccaf67efde
Chetna Miittal <chetna.mittal125@gmail.com>
2026-09-18 20:18:47 +0530

    chore: upstream changes

--- packages/testing/src/execution_testing/cli/pytest_commands/plugins/forks/forks.py
+++ packages/testing/src/execution_testing/cli/pytest_commands/plugins/forks/forks.py
@@ -124,7 +124,7 @@ class ForkParametrizer:
             marks = []
         self.fork_covariant_parameters = [
             ForkCovariantParameter(
-                names=["parametrized_fork"],
+                names=["fork"],
                 values=[
                     pytest.param(
                         fork,
@@ -716,7 +716,7 @@ def pytest_report_header(config: pytest.Config, start_path: Any) -> List[str]:
 
 
 @pytest.fixture(autouse=True)
-def parametrized_fork(request: pytest.FixtureRequest) -> None:
+def fork(request: pytest.FixtureRequest) -> None:
     """Parametrize test cases by fork."""
     pass
 
@@ -1316,9 +1316,7 @@ def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
                     ],
                 )
             ]
-            metafunc.parametrize(
-                "parametrized_fork", pytest_params, scope="function"
-            )
+            metafunc.parametrize("fork", pytest_params, scope="function")
         return
 
     # Get the intersection between the test's validity marker and the current
@@ -1327,7 +1325,7 @@ def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
         test_fork_set & metafunc.config.selected_fork_set  # type: ignore
     )
 
-    if "parametrized_fork" not in metafunc.fixturenames:
+    if "fork" not in metafunc.fixturenames:
         return
 
     intersection_set -= get_unsupported_forks(metafunc.config)
@@ -1347,9 +1345,7 @@ def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
                     ],
                 )
             ]
-            metafunc.parametrize(
-                "parametrized_fork", pytest_params, scope="function"
-            )
+            metafunc.parametrize("fork", pytest_params, scope="function")
     else:
         pytest_params = []
         for fork in sorted(intersection_set):
@@ -1662,7 +1658,7 @@ def pytest_collection_modifyitems(
                 continue
 
         # --- validity markers ---
-        fork = params.get("parametrized_fork")
+        fork = params.get("fork")
         if fork is None:
             continue
 
